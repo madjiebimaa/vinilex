561e8efb4e45454eb9b28b79cc8935339e5e6955
Muhammad Adjie Bimaditya <madjiebimaa@gmail.com>
2024-01-12 15:40:57 +0700

    style: reposition bubbles in color detail page

--- src/components/ClosestColorAreas.tsx
+++ src/components/ClosestColorAreas.tsx
@@ -1,31 +1,16 @@
-import { Home } from 'lucide-react';
-import { NavLink, useNavigate } from 'react-router-dom';
-
-import CopyButton from './CopyButton';
-import HeartButton from './HeartButton';
-import { Button, ButtonProps, buttonVariants } from './ui/button';
+import { NavLink } from 'react-router-dom';
 
 import { Color } from '@/lib/types';
-import { cn, getTopNClosestColors } from '@/lib/utils';
+import { getTopNClosestColors } from '@/lib/utils';
 import { useColors } from '@/store/color';
 
-const areas: string[] = [
-  'one',
-  'two',
-  'three',
-  'four',
-  'five',
-  'home',
-  'heart',
-  'copy',
-];
+const areas: string[] = ['one', 'two', 'three', 'four', 'five'];
 
 interface ClosestColorAreasProps {
   color: Color;
 }
 
 export default function ClosestColorAreas({ color }: ClosestColorAreasProps) {
-  const navigate = useNavigate();
   const colors = useColors();
 
   const closestColors = getTopNClosestColors(color, colors);
@@ -35,63 +20,19 @@ export default function ClosestColorAreas({ color }: ClosestColorAreasProps) {
     ...closestColors[index],
   }));
 
-  const toolStyles: ButtonProps = {
-    variant: 'secondary',
-    size: 'icon',
-    className: 'rounded-full hover:brightness-90 transition-all duration-300',
-  };
-
-  const tools: {
-    Tool: React.ReactNode;
-  }[] = [
-    {
-      Tool: (
-        <Button {...toolStyles} onClick={() => navigate('/')}>
-          <Home className="shrink-0 h-4 w-4" />
-        </Button>
-      ),
-    },
-    {
-      Tool: <HeartButton {...toolStyles} colorID={color.id} />,
-    },
-    {
-      Tool: <CopyButton {...toolStyles} text={color.hexCode} />,
-    },
-  ];
-
-  const toolAreas = areas.slice(5, areas.length).map((area, index) => ({
-    area,
-    ...tools[index],
-  }));
-
   return (
     <section
       style={{
         gridTemplateAreas: `
-        "hexCode hexCode hexCode hexCode hexCode two two two home home"
-        "hexCode hexCode hexCode hexCode hexCode two two two home home"
-        "one one one one one two two two heart heart"
-        "one one one one one three three four heart heart"
-        "one one one one one three three five copy copy"
-        "one one one one one a a a copy copy"
-        "one one one one one a a a b b"
+        "one one one one one two   two   two  five c"
+        "one one one one one two   two   two  b    c"
+        "one one one one one two   two   two  b    c"
+        "one one one one one three three four b    c"
+        "one one one one one three three a    b    c"
       `,
       }}
-      className="grid grid-cols-[repeat(10,_20px)] grid-rows-[repeat(7,_20px)] gap-2"
+      className="grid grid-cols-[repeat(10,_20px)] grid-rows-[repeat(5,_20px)] gap-2"
     >
-      <div
-        style={{ gridArea: 'hexCode' }}
-        className="flex justify-center items-center w-fit p-1 rounded-full shadow-md bg-slate-100"
-      >
-        <p
-          className={cn(
-            buttonVariants({ variant: 'ghost', size: 'sm' }),
-            'h-10 rounded-full bg-white font-sans font-semibold hover:bg-white'
-          )}
-        >
-          {color.hexCode}
-        </p>
-      </div>
       {colorAreas.map((colorArea) => (
         <NavLink
           key={colorArea.id}
@@ -102,15 +43,6 @@ export default function ClosestColorAreas({ color }: ClosestColorAreasProps) {
           }}
           className="rounded-full shadow-md"
         />
-      ))}
-      {toolAreas.map(({ area, Tool }) => (
-        <div
-          key={area}
-          style={{ gridArea: area }}
-          className="flex justify-center items-center p-1 rounded-full shadow-md bg-slate-100"
-        >
-          {Tool}
-        </div>
       ))}
     </section>
   );
--- src/routes/color.tsx
+++ src/routes/color.tsx
@@ -1,8 +1,10 @@
-import { ArrowLeft } from 'lucide-react';
+import { ArrowLeft, Home } from 'lucide-react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import ClosestColorAreas from '@/components/ClosestColorAreas';
-import { Button, buttonVariants } from '@/components/ui/button';
+import CopyButton from '@/components/CopyButton';
+import HeartButton from '@/components/HeartButton';
+import { Button, ButtonProps, buttonVariants } from '@/components/ui/button';
 
 import { cn } from '@/lib/utils';
 import { useColors } from '@/store/color';
@@ -13,7 +15,47 @@ export default function Color() {
   const colors = useColors();
 
   const color = colors.find((color) => color.id === colorID)!;
+
+  const bubbleButtonStyles: ButtonProps = {
+    variant: 'secondary',
+    size: 'icon',
+    className: 'rounded-full hover:brightness-90 transition-all duration-300',
+  };
+
+  const bubbleLabelStyles = {
+    className: cn(
+      buttonVariants({ variant: 'ghost', size: 'sm' }),
+      'h-10 rounded-full bg-white font-sans font-semibold hover:bg-white'
+    ),
+  };
+
+  const bubbleWrapperStyles = {
+    className:
+      'flex justify-center items-center p-1 rounded-full shadow-md bg-slate-100',
+  };
+
   const colorLabels = [color.name, color.code];
+
+  const bubbles: {
+    Bubble: React.ReactNode;
+  }[] = [
+    {
+      Bubble: (
+        <Button {...bubbleButtonStyles} onClick={() => navigate('/')}>
+          <Home className="shrink-0 h-4 w-4" />
+        </Button>
+      ),
+    },
+    {
+      Bubble: <p {...bubbleLabelStyles}>{color.hexCode}</p>,
+    },
+    {
+      Bubble: <CopyButton {...bubbleButtonStyles} text={color.hexCode} />,
+    },
+    {
+      Bubble: <HeartButton {...bubbleButtonStyles} colorID={color.id} />,
+    },
+  ];
 
   return (
     <main
@@ -21,31 +63,29 @@ export default function Color() {
       className="h-screen w-screen flex flex-col p-4 gap-4"
     >
       <nav className="flex items-center gap-2">
-        <div className="flex justify-center items-center p-1 rounded-full shadow-md bg-slate-100">
-          <Button
-            type="button"
-            variant="secondary"
-            size="icon"
-            className="rounded-full hover:brightness-90 transition-all duration-300"
-            onClick={() => navigate(-1)}
-          >
+        <div {...bubbleWrapperStyles}>
+          <Button {...bubbleButtonStyles} onClick={() => navigate(-1)}>
             <ArrowLeft className="shrink-0 h-4 w-4" />
           </Button>
         </div>
-        <div className="flex justify-center items-center gap-4 p-1 rounded-full shadow-md bg-slate-100">
+        <div className={cn(bubbleWrapperStyles.className, 'gap-2')}>
           {colorLabels.map((label) => (
-            <p
-              key={label}
-              className={cn(
-                buttonVariants({ variant: 'ghost', size: 'sm' }),
-                'h-10 rounded-full bg-white font-sans font-semibold hover:bg-white'
-              )}
-            >
+            <p key={label} {...bubbleLabelStyles}>
               {label}
             </p>
           ))}
         </div>
       </nav>
+      <div className="flex items-center gap-2">
+        {bubbles.map(({ Bubble }, index) => (
+          <div
+            key={`${color.id}_bubble_button_${index}`}
+            {...bubbleWrapperStyles}
+          >
+            {Bubble}
+          </div>
+        ))}
+      </div>
       <ClosestColorAreas color={color} />
     </main>
   );
